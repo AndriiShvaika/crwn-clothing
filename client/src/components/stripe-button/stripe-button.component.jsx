8da7bdb92531a9771a7fa72e83a573af879cbea9
Andrii <shvaika.andrii@gmail.com>
2021-07-22 12:02:52 +0300

    added back-end part for stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,4 +1,5 @@
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -6,8 +7,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51JCjJYCXC8K5iXgB45cK8lym6KM19C64iYLQqoK2smg7w8fJTfdDLi7iDaE8AX0w8L6LtaEM2TVHKYcSjflgSemn00fTDAZY3L";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment succesful");
+      })
+      .catch((error) => {
+        console.log("Payment error:", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. \nPlease make sure you used the provided credit card."
+        );
+      });
   };
 
   return (
